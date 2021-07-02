3046e92002b6e854034d0d41e6b7fb9d974e9a84
Tommy Olsson <enzeru@gmail.com>
2021-07-02 10:03:47 +0200

    EverCraft project setup

---
+++ EverCraft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EverCraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
