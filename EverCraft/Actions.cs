e5824ea898ff1a599d4122e5e61051ac17e1aa87
Tommy Olsson <enzeru@gmail.com>
2021-08-10 10:54:28 +0200

    Attacking and basic actions setup

---
+++ EverCraft/Actions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace EverCraft
+{
+    public class Actions
+    {
+        public int Result { get; set; }
+        public void Attack()
+        {
+            SkillCheck();
+            var attackRoll = Result;
+        }
+
+        public void SkillCheck()
+        {
+            Result = 15;
+        }
+    }
+}
