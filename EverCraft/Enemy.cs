2337881c92d3ae3c607a26becb3e1f3d1a7c689c
Tommy Olsson <enzeru@gmail.com>
2021-08-10 10:19:18 +0200

    Enemy class added

---
+++ EverCraft/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EverCraft
+{
+    class Enemy
+    {
+        public int hitPoints = 3;
+        public int armorClass = 11;
+        public int attackDamage = 0;
+        public int attackRoll = 0;
+
+        public string Name
+        { get; set; }
+    }
+}
