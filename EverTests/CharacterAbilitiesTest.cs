8a6d9af1359a28b598efa097031d06e2664d63a4
Chris Bingham <christopher.bingham@marken.com>
2021-11-15 22:43:58 +0000

    start abilities test

---
+++ EverTests/CharacterAbilitiesTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EverTests
+{
+    [TestClass]
+    public class CharacterAbilitiesTest
+    {
+        // Abilities are Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma
+        // Abilities range from 1 to 20 and default to 10
+        // Abilities have modifiers according to the following table
+
+        [TestMethod]
+        public void TestAbilities()
+        {
+           var character = new Character("Abel");
+           
+        }
+
+    }
+}
